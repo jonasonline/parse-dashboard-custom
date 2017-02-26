9f1e594cf2ffef85bbe4e85c97526734445c3096
Jonas Carlsson <jonas@jonasonline.se>
2017-02-26 18:27:18 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var ParseDashboard = require('parse-dashboard');
+
+var dashboard = new ParseDashboard({
+  apps: [
+    {
+      appId: process.env.APP_ID || 'myAppId',
+      masterKey: process.env.MASTER_KEY || 'myMasterKey',
+      serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
+      appName: process.env.APP_NAME || 'MyApp',
+    },
+  ],
+});
+
+var app = express();
+
+// make the Parse Dashboard available at /
+app.use('/', dashboard);
+
+var port = process.env.PORT || 4040;
+var httpServer = require('http').createServer(app);
+httpServer.listen(port, function() {
+  console.log('parse-dashboard-example running on port ' + port + '.');
+});
